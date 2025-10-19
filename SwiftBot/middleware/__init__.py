0fea206cc6c0ed2d7ec82ded09c3d2a2849fbd44
Arjun <86394506+Arjun-M@users.noreply.github.com>
2025-10-19 17:14:45 +0530

    Add middleware components to __init__.py

---
+++ SwiftBot/middleware/__init__.py
@@ -0,0 +1,12 @@
+"""
+Middleware components
+Copyright (c) 2025 Arjun-M/SwiftBot
+"""
+
+from .base import Middleware
+from .logger import Logger
+from .rate_limiter import RateLimiter
+from .auth import Auth
+from .user_data import UserDataMiddleware
+
+__all__ = ["Middleware", "Logger", "RateLimiter", "Auth", "UserDataMiddleware"]
