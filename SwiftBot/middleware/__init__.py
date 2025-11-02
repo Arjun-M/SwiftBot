b0b5719a8415b38210799dfabc76f12d86131b39
Arjun <86394506+Arjun-M@users.noreply.github.com>
2025-11-02 16:53:15 +0530

    Update __init__.py

--- SwiftBot/middleware/__init__.py
+++ SwiftBot/middleware/__init__.py
@@ -1,13 +1,18 @@
 """
-Middleware components
+SwiftBot Middleware System
 Copyright (c) 2025 Arjun-M/SwiftBot
 """
 
 from .base import Middleware
 from .logger import Logger
-from .rate_limiter import RateLimiter
 from .auth import Auth
-from .user_data import UserDataMiddleware
+from .rate_limiter import RateLimiter
 from .analytics import AnalyticsCollector
 
-__all__ = ["Middleware", "Logger", "RateLimiter", "Auth", "UserDataMiddleware" , "AnalyticsCollector"]
+__all__ = [
+    'Middleware',
+    'Logger',
+    'Auth', 
+    'RateLimiter',
+    'AnalyticsCollector'
+]
